b40efc1244642dbcfc7e3cd108f88d2cd65265ec
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-08-08 15:14:54 -0400

    cleanup

--- server.js
+++ server.js
@@ -6,33 +6,24 @@ var fetch = require("node-fetch");
 const Observable = require("rxjs/Observable");
 
 var observable = Observable.Observable.create(observer => {
-  setInterval(() => {
-    fetch(`https://api.iextrading.com/1.0/tops?symbols=SNAP,fb,AIG%2b`)
-      .then(response => response.text())
-      .then(response => {
-        observer.next("this is observer");
-        observer.next(response);
-        io.emit("quoteResponse", response);
-      });
-  }, 3000);
+  try {
+    setInterval(() => {
+      fetch(`${endpoint}/tops?symbols=fb`)
+        .then(response => response.json())
+        .then(response => {
+          observer.next(response);
+          io.emit("quoteResponse", response[0]);
+        });
+    }, 5000);
+  } catch (err) {
+    observer.error(err);
+  }
 });
 
-observable.subscribe(x => console.log(x), x => console.log(x));
+observable.subscribe(x => console.log(x), error => console.log("ERROR!!!"));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
-});
-
-io.on("connection", socket => {
-  console.log("someone connected on the socket yo");
-  //   socket.on("clientRequestStock", quote => {
-  //     fetch(`https://api.iextrading.com/1.0/${quote}`)
-  //       .then(response => response.text())
-  //       .then(response => {
-  //         console.log(response);
-  //         io.emit("quoteResponse", response);
-  //       });
-  //   });
 });
 
 http.listen(3000);
